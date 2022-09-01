4b9be918c467ff5a161b4f2ccb06b1ea4165e63d
Pepe <jo.ve.or.84@gmail.com>
2022-09-01 11:57:21 -0500

    Day 1: Project - Brewing Company Name Generator

---
+++ day_1/project_brewery.py
@@ -0,0 +1,4 @@
+# Brewing Company Name Generator
+favorite_color = input("What is your favorite color?: ")
+favorite_animal = input("What is your favorite animal?: ")
+print(f"Congratulations, the name of your brand new beer company will be \"The {favorite_color} {favorite_animal}'s\" Brewing Company")
